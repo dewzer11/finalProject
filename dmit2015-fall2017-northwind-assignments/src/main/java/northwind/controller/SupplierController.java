58c070a42fb99217147b907864963c357c511d06
Tyson Dewsbery <dewsberyt@gmail.com>
2017-09-28 15:47:11 -0600

    Customer and Supplier Controllers

---
+++ dmit2015-fall2017-northwind-assignments/src/main/java/northwind/controller/SupplierController.java
@@ -0,0 +1,29 @@
+package northwind.controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import northwind.data.SupplierRepository;
+import northwind.model.Supplier;
+
+@Model
+public class SupplierController {
+
+	@Inject
+	private SupplierController supplierRepository;
+	
+	private List<Supplier> suppliers;
+	
+	@PostConstruct
+	void init() {
+		suppliers = supplierRepository.findAll();
+	}
+
+	public List<Supplier> getCustomers() {
+		return suppliers;
+	}
+	
+}
