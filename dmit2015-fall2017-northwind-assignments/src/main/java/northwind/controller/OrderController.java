ada107483dfe1f41a5a7a2af9b22fa5b4520f6ab
dmit2015 <dmit2015@ubuntu>
2017-10-05 16:50:37 -0600

    Get orders by cutomers done

--- dmit2015-fall2017-northwind-assignments/src/main/java/northwind/controller/OrderController.java
+++ dmit2015-fall2017-northwind-assignments/src/main/java/northwind/controller/OrderController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+
+import org.omnifaces.util.Messages;
 
 import northwind.data.OrderRepository;
 import northwind.model.Order;
@@ -25,5 +28,38 @@ public class OrderController {
 	public List<Order> getOrders() {
 		return orders;
 	}
+	
+	private List<Order> ordersByCustomer;	// getter
+	private String currentSelectedCustomerId;	// getter/setter
+	
+	public void findOrderByCustomer() {
+		if( !FacesContext.getCurrentInstance().isPostback() ) {
+			// verify that a valid genreId was set
+			if( currentSelectedCustomerId != null && !currentSelectedCustomerId.isEmpty()) {
+				ordersByCustomer = orderRepository.findAllByCustomerId(currentSelectedCustomerId);
+				if( ordersByCustomer.size() == 0 ) {
+					Messages.addGlobalInfo("There are no orders for customerID {0}", 
+							currentSelectedCustomerId);
+				}
+			} else {
+				Messages.addGlobalError("Bad request. A valid customerID is required.");
+			}
+		}
+		}
+		
+		public String getCurrentSelectedCustomerId() {
+			return currentSelectedCustomerId;
+		}
 
-}
+		public void setCurrentSelectedGenreId(String currentSelectedCustomerId) {
+			this.currentSelectedCustomerId = currentSelectedCustomerId;
+		}
+
+		public List<Order> ordersByCustomer() {
+			return ordersByCustomer;
+		}
+
+		public void setCurrentSelectedCustomerId(String currentSelectedCustomerId) {
+			this.currentSelectedCustomerId = currentSelectedCustomerId;
+		}
+	}
