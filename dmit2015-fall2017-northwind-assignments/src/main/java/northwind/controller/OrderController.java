c667318a7be658d68417dd1a44ebfaf774067560
rlong14 <rlong14@studentmail.nait.ca>
2017-10-16 16:42:07 -0600

    Updated order details and then updated order controller

--- dmit2015-fall2017-northwind-assignments/src/main/java/northwind/controller/OrderController.java
+++ dmit2015-fall2017-northwind-assignments/src/main/java/northwind/controller/OrderController.java
@@ -12,8 +12,38 @@ import org.omnifaces.util.Messages;
 import northwind.data.OrderRepository;
 import northwind.model.Order;
 
+
 @Model
 public class OrderController {
+	private int currentSelectedOrderId;		// getter/setter
+	public int getCurrentSelectedOrderId() {
+		return currentSelectedOrderId;
+	}
+
+	public void setCurrentSelectedOrderId(int currentSelectedOrderId) {
+		this.currentSelectedOrderId = currentSelectedOrderId;
+	}
+
+	public Order getCurrentSelectedOrder() {
+		return currentSelectedOrder;
+	}
+	private Order currentSelectedOrder;	// getter
+	
+	public void findOrder() {
+		if( !FacesContext.getCurrentInstance().isPostback() ) {
+			if( currentSelectedOrderId > 0 ) {
+				currentSelectedOrder = orderRepository.find(currentSelectedOrderId);
+				if( currentSelectedOrder == null ) {
+					Messages.addGlobalInfo("There is no Order with OrderID {0}", 
+							currentSelectedOrderId);
+				} else {
+					Messages.addGlobalInfo("Successfully retreived Order info.");
+				}
+			} else {
+				Messages.addGlobalError("Bad request. A valid OrderID is required.");
+			}
+		}		
+	}
 	
 	@Inject
 	private OrderRepository orderRepository;
