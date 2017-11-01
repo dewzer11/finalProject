51cd28692765e98b215b47fa263e67eb96f161bb
dmit2015 <dmit2015@ubuntu>
2017-11-01 15:49:32 -0600

    Building webpage for creating categories

--- dmit2015-fall2017-northwind-assignments/src/main/java/northwind/controller/CategoriesController.java
+++ dmit2015-fall2017-northwind-assignments/src/main/java/northwind/controller/CategoriesController.java
@@ -6,9 +6,15 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.omnifaces.util.Faces;
+
+import net.bootsfaces.component.messages.Messages;
 import northwind.data.CategoriesRepository;
 import northwind.model.Category;
 import northwind.report.CategorySales1997;
+import northwind.service.CategoryService;
 
 @Model
 public class CategoriesController {
@@ -27,7 +33,25 @@ public class CategoriesController {
 		return Categories;
 	}
 	
+
+	
 	public List<CategorySales1997> retrieveCategorySales() {
 		return CategoriesRepository.findCategorySales();
+	}
+	
+	private CategoryService categoryService;
+	
+	@NotBlank(message="Categor Name value is required")
+	@Length
+	private String categoryName; // +getter+setter
+	
+	public void createNewCategory() {
+		try	{
+			categoryService.createCategory(categoryName);
+			Messages.addGlobalInfo("Create Category was successful");
+		} catch (Exception e) {
+			Messages.addGlobalError("Error creating category with exception: {0}", e.getMessage());
+			//Messages.addGlobalWarn("Create Category was not successful");
+		}
 	}
 }
